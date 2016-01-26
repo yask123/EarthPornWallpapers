0f0056ec9c0e098b9d0bc29a866a0baff84eb09f
Yask Srivastava <yask123@gmail.com>
2016-01-26 22:52:50 +0530

    Update earthporn.py

--- earthporn.py
+++ earthporn.py
@@ -2,19 +2,40 @@ import praw
 from PIL import Image
 import requests
 import shutil
-
-r = praw.Reddit(user_agent='Test Script by /u/yask')
-submissions = r.get_subreddit('EarthPorn').get_top_from_all(limit=10)
-
 import os
-i=0
-for each in submissions:
-	#im = Image.open(each.url)
-	url = each.url
-	response = requests.get(url, stream=True)
-	with open(str(i)+'.png', 'wb') as out_file:
-		shutil.copyfileobj(response.raw, out_file)
-	imagePath = os.getcwd()+'/'+str(i)+".png"	
-	os.system ('osascript -e \'tell application "Finder" to set desktop picture to POSIX file "'+imagePath+'" \'')	
-	print each.url
-	i+=1
+
+def sanitize(link):
+	if 'jpg' in link or 'png' in link: 
+		return link
+	elif 'imgur' in link:
+		return each.url+'.png'	
+	else:
+		raise ValueError ('cant sanitize url'+link)
+		return None
+
+def main():
+	r = praw.Reddit(user_agent='Test Script by /u/yask')
+	submissions = r.get_subreddit('EarthPorn').get_top_from_all(limit=10)
+
+
+	for i,each in enumerate(submissions):
+		
+		try:
+			url = sanitize(each.url)
+		except ValueError as e:
+			print e
+			continue
+		response = requests.get(url, stream=True)
+
+		# Download images in  ~/.epwallpapers directory all images
+		os.mkdir('~/.epwallpapers')
+		with open('~/.epwallpapers/'+str(i)+'.png', 'wb') as out_file:
+			shutil.copyfileobj(response.raw, out_file)
+
+		imagePath = '~/.epwallpapers/'+str(i)+'.png'
+		osxcmd = 'osascript -e \'tell application "Finder" to set desktop picture to POSIX file "'+imagePath+'" \'' 	
+		os.system (osxcmd)	
+		print each.url
+
+if __name__ == '__main__':
+	main()
