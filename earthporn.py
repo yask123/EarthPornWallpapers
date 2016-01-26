dfa92602693702e3843519a9523d14240da634f8
Yask Srivastava <yask123@gmail.com>
2016-01-27 00:42:24 +0530

    Update earthporn.py

--- earthporn.py
+++ earthporn.py
@@ -3,12 +3,12 @@ from PIL import Image
 import requests
 import shutil
 import os
-
+import time
 def sanitize(link):
 	if 'jpg' in link or 'png' in link: 
 		return link
 	elif 'imgur' in link:
-		return each.url+'.png'	
+		return link+'.png'	
 	else:
 		raise ValueError ('cant sanitize url'+link)
 		return None
@@ -16,26 +16,38 @@ def sanitize(link):
 def main():
 	r = praw.Reddit(user_agent='Test Script by /u/yask')
 	submissions = r.get_subreddit('EarthPorn').get_top_from_all(limit=10)
+	my_dir = os.path.expanduser('~/.epwallpapers')
+	
+	if not os.path.exists(my_dir):
+		os.makedirs(my_dir)
+	i=0
+	wlinks = []
+	for each in submissions:
+		wlinks.append(each)
+	wcounts = len(wlinks)
 
-
-	for i,each in enumerate(submissions):
-		
+	while True:	
+		each = wlinks[i]
 		try:
 			url = sanitize(each.url)
+			imgext = url.split('.')[-1]
 		except ValueError as e:
 			print e
 			continue
 		response = requests.get(url, stream=True)
 
 		# Download images in  ~/.epwallpapers directory all images
-		os.mkdir('~/.epwallpapers')
-		with open('~/.epwallpapers/'+str(i)+'.png', 'wb') as out_file:
+		with open(my_dir+'/'+str(i)+'.'+imgext, 'wb') as out_file:
 			shutil.copyfileobj(response.raw, out_file)
 
-		imagePath = '~/.epwallpapers/'+str(i)+'.png'
+		imagePath = my_dir+'/'+str(i)+'.'+imgext
 		osxcmd = 'osascript -e \'tell application "Finder" to set desktop picture to POSIX file "'+imagePath+'" \'' 	
 		os.system (osxcmd)	
-		print each.url
+		print 'Done'
+		i+=1
+		if i == wcounts:
+			i=0
+		time.sleep(1)
 
 if __name__ == '__main__':
 	main()
